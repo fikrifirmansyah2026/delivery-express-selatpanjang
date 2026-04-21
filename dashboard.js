c8c0b810656135c3d470cef12a0436f2f3dcef69
Fikri Firmansyah <fikrifir80@gmail.com>
2026-04-21 20:01:13 +0700

    Update dashboard.js
    Update Add initial create dashboard.js with project
    
    Signed-off-by: Fikri Firmansyah  <fikrifir80@gmail.com>

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,6 @@
-function tampilkanOrder() {
-  const list = document.getElementById("listOrder");
+function updateStatus(index, statusBaru) {
   let orders = JSON.parse(localStorage.getItem("orders")) || [];
-
-  list.innerHTML = "";
-
-  orders.forEach((order, index) => {
-    list.innerHTML += `
-      <div class="card">
-        <p><b>Nama:</b> ${order.nama}</p>
-        <p><b>Alamat:</b> ${order.alamat}</p>
-        <p><b>Barang:</b> ${order.barang}</p>
-        <p><b>Berat:</b> ${order.berat} kg</p>
-        <p><b>Status:</b> ${order.status}</p>
-      </div>
-    `;
-  });
+  orders[index].status = statusBaru;
+  localStorage.setItem("orders", JSON.stringify(orders));
+  tampilkanOrder();
 }
-
-tampilkanOrder();
