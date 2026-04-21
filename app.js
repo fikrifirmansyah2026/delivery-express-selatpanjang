72ef87deee4c47dac2e52315ad8ac6ea32b1249e
Fikri Firmansyah <fikrifir80@gmail.com>
2026-04-21 20:17:52 +0700

    Update app.js
    Update Add initial create app.js with project 
    
    Signed-off-by: Fikri Firmansyah  <fikrifir80@gmail.com>

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ function hitungOngkir() {
   document.getElementById("ongkir").innerText = "Rp " + total;
 }
 
+function ambilLokasi() {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    document.getElementById("alamat").value =
+      "Lat: " + lat + ", Lon: " + lon;
+  });
+}
+
 function hubungiKami() {
   alert("Silakan hubungi admin via WhatsApp!");
 }
