a476e14478fb853ae2d17f6ec491029d3136a8d8
Fikri Firmansyah <fikrifir80@gmail.com>
2026-05-23 03:54:32 +0700

    Add Google AdSense auto ads script
    Add Initial Create google adsense auto ads script with project
    
    Signed-off-by: Fikri Firmansyah  <fikrifir80@gmail.com>

---
+++ assets/js/ads/google-ads.js
@@ -0,0 +1,16 @@
+// ========================================
+// GOOGLE ADSENSE AUTO ADS 2026
+// ========================================
+
+const adsScript = document.createElement("script");
+
+adsScript.async = true;
+
+adsScript.src =
+"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-665da915-436c-4d05-b030-10ca1ad1abe1";
+
+adsScript.crossOrigin = "anonymous";
+
+document.head.appendChild(adsScript);
+
+console.log("Google AdSense Loaded");
